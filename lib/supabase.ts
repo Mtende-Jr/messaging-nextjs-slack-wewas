e2560a56482ec1668a2892f0f642f9a2f2f46cc1
Mtende-Jr <92046270+Mtende-Jr@users.noreply.github.com>
2025-11-19 11:48:38 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-// lib/supabase.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY // <-- correct env var name
-)
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+);
