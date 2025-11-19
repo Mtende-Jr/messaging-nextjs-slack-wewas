e20c81c2699a3d3941ba22987d0382e27ace1c33
Mtende-Jr <92046270+Mtende-Jr@users.noreply.github.com>
2025-11-19 11:11:04 +0300

    Update [id].js

--- pages/channels/[id].js
+++ pages/channels/[id].js
@@ -1,3 +1,4 @@
+// pages/channels/[id].js
 import Layout from '~/components/Layout'
 import Message from '~/components/Message'
 import MessageInput from '~/components/MessageInput'
@@ -6,43 +7,51 @@ import { useStore, addMessage } from '~/lib/Store'
 import { useContext, useEffect, useRef } from 'react'
 import UserContext from '~/lib/UserContext'
 
-const ChannelsPage = (props) => {
+const ChannelsPage = () => {
   const router = useRouter()
-  const { user, authLoaded, signOut } = useContext(UserContext)
+  const { user, userLoaded, signOut } = useContext(UserContext)
   const messagesEndRef = useRef(null)
 
-  // Else load up the page
   const { id: channelId } = router.query
-  const { messages, channels } = useStore({ channelId })
+  const { messages, channels } = useStore({ channelId: Number(channelId) })
 
   useEffect(() => {
-    messagesEndRef.current.scrollIntoView({
+    messagesEndRef.current?.scrollIntoView({
       block: 'start',
       behavior: 'smooth',
     })
   }, [messages])
 
-  // redirect to public channel when current channel is deleted
+  // Redirect to public channel if current channel is deleted
   useEffect(() => {
-    if (!channels.some((channel) => channel.id === Number(channelId))) {
+    if (channels.length > 0 && !channels.some((channel) => channel.id === Number(channelId))) {
       router.push('/channels/1')
     }
   }, [channels, channelId])
 
-  // Render the channels and messages
+  if (!userLoaded) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <Layout channels={channels} activeChannelId={channelId}>
+    <Layout channels={channels} activeChannelId={Number(channelId)}>
       <div className="relative h-screen">
         <div className="Messages h-full pb-16">
           <div className="p-2 overflow-y-auto">
-            {messages.map((x) => (
-              <Message key={x.id} message={x} />
+            {messages.map((msg) => (
+              <Message key={msg.id} message={msg} />
             ))}
             <div ref={messagesEndRef} style={{ height: 0 }} />
           </div>
         </div>
         <div className="p-2 absolute bottom-0 left-0 w-full">
-          <MessageInput onSubmit={async (text) => addMessage(text, channelId, user.id)} />
+          <MessageInput
+            onSubmit={async (text) => {
+              if (user) {
+                await addMessage(text, Number(channelId), user.id)
+              }
+            }}
+          />
         </div>
       </div>
     </Layout>
